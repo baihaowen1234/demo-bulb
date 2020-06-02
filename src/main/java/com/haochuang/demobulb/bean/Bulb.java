e693f65effe69714288afa0afdd87d28e3900d22
Administrator <1416196787@qq.com>
2020-06-02 14:23:01 +0800

    bean灯泡功能

--- src/main/java/com/haochuang/demobulb/bean/Bulb.java
+++ src/main/java/com/haochuang/demobulb/bean/Bulb.java
@@ -1,4 +1,28 @@
 package com.haochuang.demobulb.bean;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/*单开、
+单关
+、开/关、
+调功率、
+调节最大亮度上限值（0%~100%）、
+定时开/关。
+*/
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Bulb {
+    private String  noeopen;
+    private String  noeclose;
+    private String  open;
+    private String  close;
+    private Integer power;
+    private Integer luminance;
+    private String timingopen;
+    private String timingclose;
+
+
 }
