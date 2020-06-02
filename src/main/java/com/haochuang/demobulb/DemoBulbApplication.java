dec40834d3c274ae81917d31e6527eb14f668084
Administrator <1416196787@qq.com>
2020-06-02 13:30:20 +0800

    智能家居灯泡模块

---
+++ src/main/java/com/haochuang/demobulb/DemoBulbApplication.java
@@ -0,0 +1,13 @@
+package com.haochuang.demobulb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBulbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoBulbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/haochuang/demobulb/DemoBulbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haochuang.demobulb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBulbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
