91e1c9173b62d6c993ddd347c42b6445d29568d0
shenhao <1204317466@qq.com>
2020-06-03 13:57:24 +0800

    yuyu

--- src/main/java/com/haochuang/demobulb/ee.java
+++ src/main/java/com/haochuang/demobulb/ee.java
@@ -1,8 +1,13 @@
 package com.haochuang.demobulb;
 
+import sun.misc.PostVMInitHook;
+
 /**
  * Author Shen Hao
  * Created by Shen Hao on 2020/6/3
  */
 public class ee {
+    public void ii(){
+
+    }
 }
