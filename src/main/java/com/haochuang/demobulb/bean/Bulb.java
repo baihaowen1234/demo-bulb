e1cc32db44e806ee2ae5f266636ce6b8e4cef33e
shenhao <1204317466@qq.com>
2020-06-03 14:46:04 +0800

    修改实体类和mapper

--- src/main/java/com/haochuang/demobulb/bean/Bulb.java
+++ src/main/java/com/haochuang/demobulb/bean/Bulb.java
@@ -17,14 +17,16 @@ import lombok.NoArgsConstructor;
 public class Bulb {
     //设备ID
     private Integer id;
+    //设备名称
+    private String deviceName;
     //、开/关、
     private Integer  isOppen;
     //设备在线状态
-    private Integer isonline;
+    private Integer isOnline;
     //瓦数
     private Float  wattage ;
     //设备开启时间
-    private String timingopen;
+    private String timingOpen;
     //设备详情
     private String particulars;
 
